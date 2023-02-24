6c9f8bd9f3fedb94541a298bdf661b475bc73555
badge hunt <64038298+badgeminer@users.noreply.github.com>
2023-02-24 23:22:30 +0000

    more

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ pygame.init()
 
 scr = pygame.display.set_mode((400, 300))
 csr = None
+csrOff = 9
 
 class Block(pygame.sprite.Sprite):
 
@@ -25,27 +26,33 @@ class Block(pygame.sprite.Sprite):
        self.rect = self.image.get_rect()
        self.rect.center = xy
 
+class itm:
+   def __init__(self,tex,off=9):
+     self.tex = tex
+     self.off = off
 
 class menu:
-  def __init__(self,menus:list[pygame.Surface]):
+  def __init__(self,menus:list[itm]):
     self.menus = []
     I = 0
     for i in menus:
-      r = i.get_rect()
+      r = i.tex.get_rect()
       r.top = 5+(I*18)
       r.left = 5
       self.menus.append((r,i))
       I += 1
   def draw(self,scr):
     for i in self.menus:
-      scr.blit(i[1],i[0])
+      scr.blit(i[1].tex,i[0])
   def click(self,pos):
-    global csr
+    global csr,csrOff
     for i in self.menus:
       if i[0].collidepoint(pos):
-        csr = i[1]
+        csr = i[1].tex
+        csrOff = i[1].off
         return True
     return False
+
 
 
 
@@ -54,33 +61,57 @@ pygame.display.set_caption('Hello World!')
 a = pygame.image.load("a.png")
 p = pygame.image.load("p.png")
 e = pygame.image.load("e.png")
+powere = pygame.image.load("Pe.png")
 f = pygame.image.load("f.png")
+w = pygame.image.load("w.png")
+j = pygame.image.load("j.png")
+o = pygame.image.load("o.png")
+prism = pygame.image.load("prism.png")
 er = e.get_rect()
 
-m = menu([a,f,e])
+m = menu([
+  itm(a),
+  itm(f),
+  itm(w),
+  itm(j),
+  itm(o),
+  itm(e),
+  itm(powere),
+  itm(prism,7)
+])
 
 csr = e
 g = pygame.sprite.Group()
 csrS = Block(csr,(0,0))
+
+draw = pygame.sprite.LayeredUpdates(csrS)
+
 while True:
     k_b = pygame.key.get_pressed()
     scr.fill((0,0,0))
-    g.draw(scr)
+    draw.draw(scr)
     m.draw(scr)
     mp = pygame.mouse.get_pos()
-    er.center = mp
-    if k_b[pygame.K_LALT]:
+    csrS.rect.center = mp
+    if k_b[pygame.K_LSHIFT]:
       c = pygame.sprite.spritecollideany(csrS,g)
       if c:
-        er.centerx = c.rect.centerx
-        er.centery = c.rect.centery + 14
+        csrS.rect.center = (c.rect.centerx,c.rect.centery + csrOff)
+      else:
+        csrS.rect.center = (0,0)
         
-    scr.blit(csr,er)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
           if not m.click(event.pos):
-            g.add(Block(csr,event.pos))
+            s = Block(csr,csrS.rect.center)
+            g.add(s)
+            draw.add(s)
+            draw.move_to_back(s)
+          else:
+            csrS.image = csr
+
+            
     pygame.display.update()
